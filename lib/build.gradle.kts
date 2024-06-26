0e1526f910e3b06b0e2a0157c8dc02da408ce71c
Luca Tremamunno <tremaluca96@gmail.com>
2024-06-26 16:20:06 +0200

    Bump version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -19,7 +19,7 @@ val repoUsername: String by project
 val repoPassword: String by project
 val mavenReleasesRepository: String by project
 
-project.version = "1.0.5"
+project.version = "1.0.6"
 
 val localProperties = Properties().apply {
 	kotlin.runCatching {
