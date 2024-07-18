89b7d33525c74c1d926f796999e582ae909103cb
Clément VANDENDAELEN <cv@icure.com>
2024-07-18 17:03:19 +0200

    chore: Version bump

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -19,7 +19,7 @@ val repoUsername: String by project
 val repoPassword: String by project
 val mavenReleasesRepository: String by project
 
-project.version = "1.0.6"
+project.version = "1.1.0"
 
 val localProperties = Properties().apply {
 	kotlin.runCatching {
